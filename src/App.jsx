70a6ae311b198e8adf023bbf700a2d71ca75227d
Benson Chen <tumlivein@gmail.com>
2025-05-18 23:02:32 +0800

    Finish client side navigate by Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function Home() {
@@ -10,14 +10,37 @@ function Page1() {
 }
 
 function Route({ path, component: Component }) {
-  const [curPath] = useState(window.location.pathname);
+  const [curPath, setCurPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handleClientSideNavigate = () => {
+      setCurPath(window.location.pathname);
+    }
+
+    window.addEventListener('client-side-navigate', handleClientSideNavigate);
+  }, []);
 
   return curPath === path ? <Component /> : null;
+}
+
+function Link({ to, children }) {
+  const clientSideNavigate = (e) => {
+    e.preventDefault();
+    window.history.pushState({}, '', to);
+
+    const event = new PopStateEvent('client-side-navigate');
+    window.dispatchEvent(event);
+  }
+
+  return <a href={to} onClick={clientSideNavigate}>{children}</a>
 }
 
 function App() {
   return (
     <>
+      <Link to="/">Home</Link>
+      {` | `}
+      <Link to="/page-1">Page1</Link>
       <Route path="/" component={Home} />
       <Route path="/page-1" component={Page1} />
     </>
